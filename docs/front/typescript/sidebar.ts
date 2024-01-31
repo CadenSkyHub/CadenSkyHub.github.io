d9a13c5e64f9e318ace77e2f05a4721174f6f2ac
caden <caden@hub.com>
2024-01-31 18:15:13 +0800

    new learn

---
+++ docs/front/typescript/sidebar.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "vitepress";
+
+export const typescriptSideBar: DefaultTheme.SidebarItem[] = [
+    { text: '开始', link: '/front/typescript/1_begin.md' },
+    { text: '数据类型', link: '/front/typescript/2_data_type' },
+    { text: '联合类型', link: '/front/typescript/3_union_types' },
+    { text: '数组', link: '/front/typescript/4_type_of_array' },
+    { text: '对象', link: '/front/typescript/5_type_of_object' },
+    { text: '函数', link: '/front/typescript/6_type_of_function' },
+    { text: '接口', link: '/front/typescript/7_interface' },
+    { text: '类', link: '/front/typescript/8_class' },
+    { text: '泛型', link: '/front/typescript/9_generics' },
+    { text: '枚举', link: '/front/typescript/10_enum' },
+    { text: '断言', link: '/front/typescript/11_type_of_assertion' },
+]
