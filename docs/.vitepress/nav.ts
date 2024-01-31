ba699b5fcd47fbf7cde24c35788224c77328ef5e
caden <caden@hub.com>
2024-01-31 18:14:14 +0800

    增加 typescript

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -4,9 +4,10 @@ export const nav: DefaultTheme.NavItem[] = [
     { text: '首页', link: 'index' },
     { text: 'VitePress', link: '/vitepress/1_begin/1_begin' },
     {
-        text: '前端', 
+        text: 'JavaScript', 
         items: [
-            {text:'JavaScript', link:'/front/javascript/1_begin/1_var_const_ident'}
+            {text:'JavaScript', link:'/front/javascript/1_begin/1_var_const_ident'},
+            {text:'TypeScript', link:'/front/typescript/1_begin'}
         ]
     }
 ]
