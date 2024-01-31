60fbfc4bce082412c629dc10e58584ac3006bb5a
CadenSky <cadenSky@outlook.com>
2024-01-31 09:24:50 +0800

    更新语言

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { themeConfig } from "./theme";
 
 export default defineConfig({
     title: '< ~/ > MyNote',
+    lang: 'zh',
     description: '这里记录了我的个人笔记，因为脑子不好，所以经常查询',
     head: [
         ['link', { rel: 'icon', href: '/favicon.ico' }]
