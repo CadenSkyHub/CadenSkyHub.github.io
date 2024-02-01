4fcb7449545bbbf9e393a6ea201fa22e36130890
caden <caden@hub.com>
2024-02-01 14:51:39 +0800

    新增 node

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -7,7 +7,8 @@ export const nav: DefaultTheme.NavItem[] = [
         text: 'JavaScript', 
         items: [
             {text:'JavaScript', link:'/front/javascript/1_begin/1_var_const_ident'},
-            {text:'TypeScript', link:'/front/typescript/1_begin'}
+            {text:'TypeScript', link:'/front/typescript/1_begin'},
+            {text:'Node', link:'/front/node/node'},
         ]
     }
 ]
