2e2c405fbbb7d937bcd1ed297b74084f5e48634d
caden <caden@caden.com>
2024-09-04 20:52:25 +0800

    新增布局

--- docs/front/nuxt/sideBar.ts
+++ docs/front/nuxt/sideBar.ts
@@ -8,6 +8,7 @@ const nuxtSideBar:DefaultTheme.SidebarItem[] = [
             {text:'错误', link: '/front/nuxt/1_error'},
             {text:'配置', link: '/front/nuxt/2_config'},
             {text:'路由', link: '/front/nuxt/3_router'},
+            {text:'布局', link: '/front/nuxt/3_layouts'},
             {text:'数据获取', link: '/front/nuxt/4_get'},
             {text:'服务端', link: '/front/nuxt/5_server'},
             {text:'redis', link: '/front/nuxt/6_redis'},
